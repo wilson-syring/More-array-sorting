13fa4cb7365ceb741cfb0be6fdbdbc5576fb5da6
Wilson Syring <wilson.syring@gmail.com>
2023-03-27 16:58:13 -0500

    array sorted and displayed on th html file

--- external.js
+++ external.js
@@ -2,15 +2,13 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 //function for filtering past the articles
-function clean(bandname){
-    return bandname.replace(/^(a |the |an )/i, '').trim();
+function clean(bandName){
+    return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
 //sorted array
-const sortedBands = bands.sort(function (a,b){
-    if (a > b){
-        return 1;
-    } else {
-        return -1;
-    }
-});
+const sortedBands = bands.sort(((a,b) => clean(a) > clean(b) ? 1 : -1));
+
+// selectors for grabbing the html element and displaying the sorted array
+document.querySelector('#bands').innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+
